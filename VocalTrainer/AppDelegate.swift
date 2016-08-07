c39a5f0065398f9ba375c0b8b0cf344f53939f86
Semen Tykhonenko <stykhonenko@cogniance.com>
2016-08-08 02:18:43 +0300

    Initial Commit

---
+++ VocalTrainer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTrainer
+//
+//  Created by Semyon Tikhonenko on 8/8/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
