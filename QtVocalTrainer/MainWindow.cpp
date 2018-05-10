0a4cc53cfcaf69769fccaacdc43d03f7da58d65d
sword1202 <payoneermap1202@gmail.com>
2018-05-10 15:06:49 +0300

    MainWindow small fix

--- QtVocalTrainer/MainWindow.cpp
+++ QtVocalTrainer/MainWindow.cpp
@@ -1,5 +1,9 @@
 #include "MainWindow.h"
 
+#ifndef __APPLE__
+MainWindow::MainWindow() {}
+#endif
+
 void MainWindow::resizeEvent(QResizeEvent *event) {
     QQuickView::resizeEvent(event);
     emit resize();
