92785ee78b45f2d147b42844566602f12816d399
sword1202 <payoneermap1202@gmail.com>
2018-05-19 11:33:20 +0300

    executeOnMainThread allocators added

--- QtVocalTrainer/app.cpp
+++ QtVocalTrainer/app.cpp
@@ -1,12 +1,23 @@
 #include "app.h"
 #include "../PitchDetection/PortAudio.h"
+#include <boost/pool/pool_alloc.hpp>
 
 static const QEvent::Type MainLoopEvent = (QEvent::Type)(QEvent::User + 1);
 
 class MainLoopCallbackEvent : public QEvent {
+    // avoid intensive memory allocation
+    static boost::fast_pool_allocator<MainLoopCallbackEvent> allocator;
 public:
     std::function<void()> callback;
     MainLoopCallbackEvent(const std::function<void()>& callback) : QEvent(MainLoopEvent), callback(callback) {
+    }
+
+    void* operator new(size_t size) {
+        return allocator.allocate();
+    }
+
+    void operator delete(void * p) {
+        allocator.destroy((MainLoopCallbackEvent*)p);
     }
 };
 
