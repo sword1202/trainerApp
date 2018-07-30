2719baa633d44780fcc4a786e17bb6f49fc13b1c
sword1202 <payoneermap1202@gmail.com>
2018-07-31 01:10:46 +0300

    Some more vertical scroll adjustements

--- QtVocalTrainer/qmlzoomcontroller.h
+++ QtVocalTrainer/qmlzoomcontroller.h
@@ -15,7 +15,9 @@ public:
     Q_PROPERTY(qreal intervalHeight READ getIntervalHeight() NOTIFY zoomChanged())
     Q_PROPERTY(qreal minZoom READ getMinZoom() CONSTANT)
     Q_PROPERTY(qreal maxZoom READ getMaxZoom() CONSTANT)
-    Q_PROPERTY(qreal pageSize READ getPageSize() NOTIFY onPageSizeChanged())
+    Q_PROPERTY(qreal pageSize READ getPageSize() NOTIFY pageSizeChanged())
+    Q_PROPERTY(qreal verticalScrollPosition READ getVerticalScrollPosition() WRITE
+            setVerticalScrollPosition() NOTIFY verticalScrollPositionChanged())
     Q_PROPERTY(QmlPitch firstPitch READ getFirstPitch() NOTIFY firstPitchChanged())
 
     void setWorkspaceGridHeight(float pageSize) override;
@@ -24,7 +26,8 @@ public:
 signals:
     void zoomChanged();
     void firstPitchChanged();
-    void onPageSizeChanged();
+    void pageSizeChanged();
+    void verticalScrollPositionChanged();
 };
 
 #endif // ZOOM_CONTROLLER_H
