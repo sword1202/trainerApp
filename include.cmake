d22412c9a6f246a507e3ceaa48368faf66dc62a1
sword1202 <payoneermap1202@gmail.com>
2018-12-17 19:46:56 +0200

    include.cmake introduced

---
+++ include.cmake
@@ -0,0 +1,22 @@
+set(includes
+        include
+        CppUtils
+        Logic/AudioInput
+        Logic/Drawers
+        Logic/Manager
+        Logic/Workspace
+        Logic/Playback/Decoding
+        Logic/Playback/Base
+        Logic/Playback/Other
+        Logic/Playback/Vx
+        Logic/Playback/Mvx
+        Logic/Playback/Midi/CraigsappMidifile
+        Logic
+        Qt
+        include/nanovg
+        include/nanovg/glew)
+
+list(TRANSFORM includes PREPEND ${CMAKE_CURRENT_LIST_DIR}/)
+
+include_directories(${includes})
+
