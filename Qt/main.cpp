6f38b55d9d77fc8ddf6abd7237b6021055e6882e
sword1202 <payoneermap1202@gmail.com>
2018-12-18 01:15:22 +0200

    Use software render for qml on MacOS

--- Qt/main.cpp
+++ Qt/main.cpp
@@ -6,13 +6,20 @@
 #include "QtBridge/QtMvxPlayer.h"
 #include "Welcome/WelcomeWindow.h"
 
+// Avoid metal and opengl collision performance issues, render qml with software engine
+#ifdef __APPLE__
+#define USE_OPEN_GL false
+#else
+#define USE_OPEN_GL true
+#endif
+
 int main(int argc, char *argv[])
 {
     VxApp::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QApplication::setAttribute(Qt::AA_UseOpenGLES, false);
-    QApplication::setAttribute(Qt::AA_UseSoftwareOpenGL, false);
-    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL, true);
+    QApplication::setAttribute(Qt::AA_UseSoftwareOpenGL, !USE_OPEN_GL);
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL, USE_OPEN_GL);
 
     QCoreApplication::setOrganizationName("NeboroSoft");
     QCoreApplication::setOrganizationDomain("neborosoft.com");
