ede1e3791f91a822ef34641820da3a762f51ef71
Semyon Tikhonenko <v.semyon.tikhonenko@reddit.com>
2020-11-25 15:02:32 +0200

    VocalTrainer iOS begin

---
+++ VocalTrainerApple/iOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  VocalTraineriOS
+//
+//  Created by Semyon Tikhonenko on 11/25/20.
+//  Copyright © 2020 Semyon Tikhonenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VocalTrainerApple/iOS/VocalTrainerApp.swift
@@ -0,0 +1,18 @@
+//
+//  VocalTraineriOSApp.swift
+//  VocalTraineriOS
+//
+//  Created by Semyon Tikhonenko on 11/25/20.
+//  Copyright © 2020 Semyon Tikhonenko. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct VocalTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
